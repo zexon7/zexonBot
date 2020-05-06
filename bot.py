828202eddc83a64ce588c9fd581fce9ee348557f
zexon7 <zexon01@gmail.com>
2020-05-07 00:16:37 +0800

    update

--- bot.py
+++ bot.py
@@ -1,25 +1,45 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
 import logging
 
-#check bug
+#logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-#Bot's API token
-updater = Updater('')
-
-#for quick access
-dispatcher = updater.dispatcher
+#function
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def start(update, context):
     context.message.reply_text("Welcome to my awesome bot!")
+    logging.info('[userMessage]: %s' % context.message.text)
 
 def hello(update, context):
     context.message.reply_text('Hello, {}'.format(context.message.from_user.first_name))
+    logging.info('[userMessage]: %s' % context.message.text)
 
-#/start
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('hello', hello))
+def getUrl(update, context):
+    reply_markup = InlineKeyboardMarkup([[
+        InlineKeyboardButton("zexon7", url = "https://github.com/zexon7")
+    ]])
+    update.send_message(context.message.chat.id, "Github", reply_markup = reply_markup)
+    logging.info('[userMessage]: %s' % context.message.text)
 
 #start bot
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    #Bot's API token
+    updater = Updater('')
+
+    #for quick access
+    dp = updater.dispatcher
+
+    #handler
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('hello', hello))
+    dp.add_handler(CommandHandler('url', getUrl))
+
+    updater.start_polling()
+    updater.idle()
